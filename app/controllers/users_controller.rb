ed9f2577c945a7c3ec42a74e40766184d9d182dc
NgocSon2412 <sonnguyenk57@gmail.com>
2016-01-28 11:36:17 +0700

    finish micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,18 +9,19 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
     @user = User.new
   end
 
-   def create
+  def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Please check your email to activate your account."
+      redirect_to root_url
     else
       render 'new'
     end
